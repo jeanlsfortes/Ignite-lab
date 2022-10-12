ee0f58fbb2eba5a71ea38a3c55e0d8c6febf2e2c
jeanls-solano <jeanlucaseh10@gmail.com>
2022-10-12 19:59:18 -0400

    inport and config tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,24 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl':32,
+    },
+    colors:{
+      'preto': '#000000',
+      'vermelho':'#D91F25',
+      'cinza-200':'#202024',
+      'cinza-400':'#121214',
+      'amarelo':'#F3E627',
+      'marrom':'#271A11',
+      'branco-400':'#C4C4CC',
+      'branco-200':'#E1E1E6',
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
